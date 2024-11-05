19fad05bb164e5c7ee7b4a838e58ea7225e7633c
Om Bhor <ombhor617@gmail.com>
2024-11-06 01:24:58 +0530

    first react project

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+
+const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=6d11efe4'
+
+const movie1 = {
+
+    "Title": "Fighting, Flying and Driving: The Stunts of Spiderman 3",
+    "Year": "2007",
+    "imdbID": "tt1132238",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNTI3NDE1ZmEtMTRiMS00YTY4LTk0OGItNjY4YmI0MDM4OGM4XkEyXkFqcGdeQXVyODE2NDgwMzM@._V1_SX300.jpg"
+
+}
+
+function App() {
+    const [movies, setMovies] = useState()
+    const [searchTerm , setSearchTerm] = useState('')
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`)
+        const data = await response.json();
+        setMovies(data.Search)
+    }
+    useEffect(() => {
+        searchMovies("spiderman")
+    }, [])
+    return (
+
+        <div className="app">
+            <h1>MovieHUB</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)} />
+                <img src={SearchIcon}
+                    alt="search" onClick={() => searchMovies(searchTerm)} />
+            </div>
+            {
+                movies?.length > 0
+                    ? (
+                        <div className="container">
+                           {movies.map((movie)=>
+                        <MovieCard movie={movie}/>)}
+                        </div>
+                    ) :
+                    (
+                        <div className="empty">
+                            <h2>No Movies Found</h2>
+                        </div>
+                    )
+            }
+
+        </div>
+
+    )
+}
+
+export default App
